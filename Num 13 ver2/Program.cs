e49cab36e5d6295e89b33b6d3fbc3d125de549f4
Vasiliy <vapalados@gmail.com>
2022-09-21 21:58:09 +0700

    complite1

---
+++ Num 13 ver2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+      String number = Console.ReadLine();
+      Console.WriteLine("Имеем число! ");
+      Console.WriteLine(number);
+      if (number.Length < 3)
+      {
+          Console.WriteLine("Нет третьего числа");
+      }
+      else
+      {
+      char[] line = number.ToCharArray();
+      Console.WriteLine("Третье число: ");
+      Console.Write(line[2]);
+      }
